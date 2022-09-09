aa263c693f035d5c46ba54da77ff1df9a75f4b23
PepesGH1 <78679234+PepesGH1@users.noreply.github.com>
2022-09-09 05:40:19 +0100

    Update speedy.lua

--- speedy.lua
+++ speedy.lua
@@ -1,15 +1,3 @@
-getgenv().Settings = {
-    Speed = 1450,
-    Jump = 3,  
-}
-
-local FirstSlider = FirstPage.AddSlider("Walk Speed", {Min = 1450, Max = 12000, Def = 1450}, function(Value)
-    Settings.Speed = Value
-end)
-local FirstSlider = FirstPage.AddSlider("Jump Power", {Min = 3, Max = 25, Def = 3}, function(Value)
-    Settings.Jump = Value
-end)
-
 local old
 old = hookmetamethod(game,"__namecall",newcclosure(function(self,...)
     local Args = {...}
