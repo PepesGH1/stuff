d97df4b1227a655fc3bd96eb35e26b40c9aef502
PepesGH1 <78679234+PepesGH1@users.noreply.github.com>
2022-09-09 05:51:08 +0100

    Delete speedy.lua

--- speedy.lua
+++
@@ -1,9 +0,0 @@
-local old
-old = hookmetamethod(game,"__namecall",newcclosure(function(self,...)
-    local Args = {...}
-    local method = getnamecallmethod()
-    if tostring(self) == 'Communicator' and method == "InvokeServer" and Args[1] == "update" then
-        return Settings.Speed, Settings.Jump 
-    end
-    return old(self,...)
-end))
